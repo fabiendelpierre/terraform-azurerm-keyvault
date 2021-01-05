bf8eea0d7b61d39b34ffc5fcec33712518b68212
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-01-05 17:09:25 -0500

    allow configuring network ACL bypass and default action

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "random_string" "random" {
-  length = 5
+  length  = 5
   special = false
-  upper = false
+  upper   = false
 }
 
 resource "azurerm_key_vault" "main" {
@@ -16,10 +16,10 @@ resource "azurerm_key_vault" "main" {
   sku_name = "standard"
 
   network_acls {
-    default_action              = "Deny"
-    bypass                      = "AzureServices"
-    ip_rules                    = var.authorized_cidrs
-    virtual_network_subnet_ids  = var.authorized_subnet_ids
+    default_action             = var.network_acl_default_action
+    bypass                     = var.network_acl_bypass
+    ip_rules                   = var.network_acl_default_action == "Deny" ? var.authorized_cidrs : []
+    virtual_network_subnet_ids = var.network_acl_default_action == "Deny" ? var.authorized_subnet_ids : []
   }
 
   tags = var.tags
@@ -30,10 +30,10 @@ resource "azurerm_key_vault_access_policy" "access_policies" {
 
   key_vault_id = azurerm_key_vault.main.id
 
-  tenant_id = each.value.tenant_id
-  object_id = each.value.object_id
-  key_permissions = each.value.key_permissions
-  secret_permissions = each.value.secret_permissions
+  tenant_id               = each.value.tenant_id
+  object_id               = each.value.object_id
+  key_permissions         = each.value.key_permissions
+  secret_permissions      = each.value.secret_permissions
   certificate_permissions = each.value.certificate_permissions
-  storage_permissions = each.value.storage_permissions
+  storage_permissions     = each.value.storage_permissions
 }
--- variables.tf
+++ variables.tf
@@ -1,32 +1,50 @@
 variable "resource_group_name" {
-  type = string
+  type        = string
   description = "The name of the resource group in which to place the Key Vault"
 }
 
 variable "location" {
-  type = string
+  type        = string
   description = "The Azure region where the Key Vault instance should be created"
 }
 
 variable "authorized_entities" {
-  type = map
+  type        = map(any)
   description = "A set of Azure AD objects (users/groups) in GUID format allowed to list (but not read) the contents (secrets/keys)."
 }
 
+variable "network_acl_default_action" {
+  type    = string
+  default = "Allow"
+  validation {
+    condition     = var.network_acl_default_action == "Allow" || var.network_acl_default_action == "Deny"
+    error_message = "Accepted values for var.network_acl_default_action are either Allow or Deny."
+  }
+}
+
+variable "network_acl_bypass" {
+  type    = string
+  default = "AzureServices"
+  validation {
+    condition     = var.network_acl_bypass == "AzureServices" || var.network_acl_bypass == "None"
+    error_message = "Accepted values for var.network_acl_bypass are either AzureServices or None."
+  }
+}
+
 variable "authorized_cidrs" {
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
   description = "A list of external subnets in CIDR notation that are allowed to connect to this instance of Key Vault."
 }
 
 variable "authorized_subnet_ids" {
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
   description = "A list of Azure subnet IDs that are allowed to connect to this instance of Key Vault."
 }
 
 variable "tags" {
-  type = map
+  type = map(any)
 }
 
 variable "base_name" {
